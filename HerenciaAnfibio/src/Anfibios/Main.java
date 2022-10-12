3956b9d56a1ae0f59c4a4ddbcae53c798ddb65be
Alejandro Parraga <109982223+Al3j0o@users.noreply.github.com>
2022-10-12 17:48:47 -0500

    Update Main.java

--- HerenciaAnfibio/src/Anfibios/Main.java
+++ HerenciaAnfibio/src/Anfibios/Main.java
@@ -12,7 +12,25 @@ public class Main {
     public static void main(String[] args) {
         Rana Rene=new Rana("Cafe", "Mediano", "Macho", "Pantano", "Amolops","Akhaorum");
         System.out.println(Rene);
-    
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("¿Que desea hacer?");
+        String accion=input.nextLine();
+        
+        while (!accion.equals("")){
+            if(accion.equals("Caminar") || accion.equals("caminar")){
+                Rene.Caminar();
+            }
+            
+            else if(accion.equals("Dormir") || accion.equals("dormir")){
+                Rene.Dormir();
+            }
+            else if(accion.equals("Comer") || accion.equals("comer")){
+                Rene.Comer();
+            }
+            System.out.println("¿Que desea hacer?");
+            accion=input.nextLine();
+        }
+        
+        
     }
-    
-}

45f8cc79c43fe3867b8e3f42119116a2f19089bd
Alejandro Parraga <109982223+Al3j0o@users.noreply.github.com>
2022-11-02 16:12:37 -0500

    Add files via upload

---
+++ cliente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BACO;
+
+/**
+ *
+ * @author Al3jo
+ */
+public class cliente extends Persona {
+
+    public cliente(String nombre, String Usuario, String id, String telefono) {
+        super(nombre, Usuario, id, telefono);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
